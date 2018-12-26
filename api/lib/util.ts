503dde7f7cce90416b983e9207b5c8351a2a6b25
Boubacar S. BARRY <bouba@hackages.io>
2018-12-26 20:32:23 +0100

    refactor: cart handling using new api

--- api/lib/util.ts
+++ api/lib/util.ts
@@ -3,10 +3,15 @@ export const responseJson = (res, body = {}, status = 200) => {
     statusCode: status,
     headers: {
       'Content-Type': 'application/json',
+      // TODO: setup correct cors
+      'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify(body),
   };
+
   res.writeHead(response.statusCode, response.headers);
   res.write(response.body);
   res.end();
 };
+
+
